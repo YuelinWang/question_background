b235108fd759313721c00c527a2b492d185949f1
YuelinWang <836473734@qq.com>
2020-06-20 11:36:20 +0800

    first fix

---
+++ src/components/page/Dashboard.vue
@@ -0,0 +1,207 @@
+<template>
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="8">
+                <el-card shadow="hover" class="mgb20" style="height:160px;">
+                    <div class="user-info">
+                        <img src="../../assets/img/img.jpg" class="user-avator" alt />
+                        <div class="user-info-cont">
+                            <div class="user-info-name">{{name}}</div>
+                            <div>{{role}}</div>
+                        </div>
+                    </div>
+                </el-card>
+                <el-card shadow="hover" style="height:250px;">
+                    <div slot="header" class="clearfix">
+                        <span>本软件涉及的语言占比</span>
+                    </div>Vue
+                    <el-progress :percentage="71.3" color="#42b983"></el-progress>JavaScript
+                    <el-progress :percentage="24.1" color="#f1e05a"></el-progress>CSS
+                    <el-progress :percentage="13.7"></el-progress>HTML
+                    <el-progress :percentage="5.9" color="#f56c6c"></el-progress>
+                </el-card>
+            </el-col>
+            <el-col :span="16">
+                <el-card shadow="hover" style="height:430px;">
+                    <div slot="header" class="clearfix">
+                        <span>待办事项</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+                    </div>
+                    <el-table :show-header="false" :data="todoList" style="width:100%;">
+                        <el-table-column width="40">
+                            <template slot-scope="scope">
+                                <el-checkbox v-model="scope.row.status"></el-checkbox>
+                            </template>
+                        </el-table-column>
+                        <el-table-column>
+                            <template slot-scope="scope">
+                                <div
+                                    class="todo-item"
+                                    :class="{'todo-item-del': scope.row.status}"
+                                >{{scope.row.title}}</div>
+                            </template>
+                        </el-table-column>
+                        <el-table-column width="60">
+                            <template>
+                                <i class="el-icon-edit"></i>
+                                <i class="el-icon-delete"></i>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import Schart from 'vue-schart';
+import bus from '../common/bus';
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+            name: localStorage.getItem('ms_username'),
+            todoList: [
+                {
+                    title: '设计第一套问卷',
+                    status: false
+                },
+                {
+                    title: '录入学生信息',
+                    status: true
+                }
+            ]
+        };
+    },
+    components: {
+        Schart
+    },
+    computed: {
+        role() {
+            return this.name === 'admin' ? '管理员' : this.name === 'testt' ? '教师' : '学生';
+        }
+    },
+    methods: {
+        changeDate() {
+            const now = new Date().getTime();
+            this.data.forEach((item, index) => {
+                const date = new Date(now - (6 - index) * 86400000);
+                item.name = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+            });
+        }
+    }
+};
+</script>
+
+
+<style scoped>
+.el-row {
+    margin-bottom: 20px;
+}
+
+.grid-content {
+    display: flex;
+    align-items: center;
+    height: 100px;
+}
+
+.grid-cont-right {
+    flex: 1;
+    text-align: center;
+    font-size: 14px;
+    color: #999;
+}
+
+.grid-num {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.grid-con-icon {
+    font-size: 50px;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    line-height: 100px;
+    color: #fff;
+}
+
+.grid-con-1 .grid-con-icon {
+    background: rgb(45, 140, 240);
+}
+
+.grid-con-1 .grid-num {
+    color: rgb(45, 140, 240);
+}
+
+.grid-con-2 .grid-con-icon {
+    background: rgb(100, 213, 114);
+}
+
+.grid-con-2 .grid-num {
+    color: rgb(45, 140, 240);
+}
+
+.grid-con-3 .grid-con-icon {
+    background: rgb(242, 94, 67);
+}
+
+.grid-con-3 .grid-num {
+    color: rgb(242, 94, 67);
+}
+
+.user-info {
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #ccc;
+    margin-bottom: 20px;
+}
+
+.user-avator {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+}
+
+.user-info-cont {
+    padding-left: 50px;
+    flex: 1;
+    font-size: 14px;
+    color: #999;
+}
+
+.user-info-cont div:first-child {
+    font-size: 30px;
+    color: #222;
+}
+
+.user-info-list {
+    font-size: 14px;
+    color: #999;
+    line-height: 25px;
+}
+
+.user-info-list span {
+    margin-left: 70px;
+}
+
+.mgb20 {
+    margin-bottom: 20px;
+}
+
+.todo-item {
+    font-size: 14px;
+}
+
+.todo-item-del {
+    text-decoration: line-through;
+    color: #999;
+}
+
+.schart {
+    width: 100%;
+    height: 300px;
+}
+</style>
---
+++ src/components/page/Donate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-redpacket_fill"></i> 支持作者
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">
+                如果想用此问卷系统进行二次开发可联系本人，有偿出售代码与服务<br/>
+                本人qq：836473734<br/>
+                如果您觉得此软件不错，可以请我喝杯饮料吗~
+            </div>
+            <div>
+                <img src="http://nev.wangyuelin.xyz/img/wechat.jpg" height="500px"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
